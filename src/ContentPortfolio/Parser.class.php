a8fc0624e12072332f491952e631b74479bcce16
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2019-01-12 03:52:27 -0800

    Update Parser.class.php

--- src/ContentPortfolio/Parser.class.php
+++ src/ContentPortfolio/Parser.class.php
@@ -2,6 +2,19 @@
 
 namespace ContentPortfolio;
 
-abstract class AbstractParser{}
+abstract class AbstractParser{
 
+  public $Url;
+  
+  public abstract function bool_isThisUrlMyTypeOfContent(){}
+  
+  public abstract funcion parseContent(){}
+  
+  public function setUrl($Url){
+    $this->Url = $Url;
+  }
+
+}
+
+//This will be the parser of last resort. This is the parser for content of unknown sorce type.
 class Parser extends AbstractParser{}
