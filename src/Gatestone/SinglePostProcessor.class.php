7872481a5ebb726c500bcd6e92de180588ed0550
Ubuntu <ubuntu@ip-172-31-34-124.ec2.internal>
2019-09-05 18:07:31 +0000

    ready for Freelancer

---
+++ src/Gatestone/SinglePostProcessor.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gatestone;
+
+class SinglePostProcessor{
+
+public function parseContent($postCUrlResponse){}
+
+public function parseDateAndTime($postCUrlResponse){}
+
+public function parseSidebarAd($postCUrlResponse){}
+
+}
---
+++ tests/unit/SinglePostProcessorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class SinglePostProcessorTest extends \Codeception\TestCase\WPTestCase{
+    
+    /**
+     * @test
+     * it should be instantiatable
+     */
+    public function isShouldBeInstantialble(){
+  
+    }
+    
+}
