2a3cb7d343259bfe21955d152bbaf3f337a0b1bb
jokerrs <nemanja@joker.rs>
2019-09-19 19:38:40 +0200

    Fixed things

--- src/Gatestone/SinglePostProcessor.class.php
+++ src/Gatestone/SinglePostProcessor.class.php
@@ -33,24 +33,24 @@ class SinglePostProcessor{
         wp_update_post( $my_post );
 
         $response = htmlspecialchars($response);
-        update_post_meta( $post_id, "cUrlResponse", $response);
-        update_post_meta( $post_id, "authorString", $authorID);
+        update_post_meta( $post_id, 'cUrlResponse', $response);
+        update_post_meta( $post_id, 'authorString', $authorID);
 
     }
 
     public function processForAuthor($response){
-        file_put_contents("/var/www/html/wp-content/plugins/gatestone/temp.html", $response);
-        $tags = get_meta_tags ("/var/www/html/wp-content/plugins/gatestone/temp.html");
+        file_put_contents('/var/www/html/wp-content/plugins/gatestone/temp.html', $response);
+        $tags = get_meta_tags ('/var/www/html/wp-content/plugins/gatestone/temp.html');
         //var_dump($tags);die();
         if(isset($tags['author'])){
             $authorString = $tags['author'];
             $RemoteAuthor = new RemoteAuthor;
-           // $siteURL = site_Url();
+            // $siteURL = site_Url();
             $authorID = $RemoteAuthor->returnAuthorID($authorString);
             return $authorID;
-        }else {
-            return FALSE;
         }
+
+        return false;
     }
     /**
      * @param string $keyword
@@ -90,8 +90,7 @@ class SinglePostProcessor{
     public function parseForContent($postCUrlResponse){
         $pos = strpos($postCUrlResponse, '<!-- PLAINTEXT STRIP END -->');
         $topCutOff =substr($postCUrlResponse,$pos);
-        $bottomCutOff = strstr($topCutOff, '<div class="subscribe-title">', true);
-        return $bottomCutOff; // YOU DO NOT NEED THIS LINE. JUST RETURN LINE BEFORE THIS ONE!
+        return strstr($topCutOff, '<div class="subscribe-title">', true); // YOU DO NOT NEED THIS LINE. JUST RETURN LINE BEFORE THIS ONE!
 
     }
 
@@ -102,83 +101,78 @@ class SinglePostProcessor{
     //example: https://www.gatestoneinstitute.org/3561/europe-hijab
     //iu also stored as stub-1.html in the tests/unit directory
     public function parseDateAndTime($postCUrlResponse = null){
-      preg_match('|<time class="nocontent" datetime="([0-9\-]{10}T[0-9:]{8})" itemprop="datePublished"|', $postCUrlResponse,$matches);
-      if ($matches){
-        return $matches[1];
-      }
-      return false;
-    }
-
-    /**
-     * @param $postCUrlResponse
-     * @return array
-     */
-    public function parseTopics($postCUrlResponse = null){
-      preg_match('|<span[^>]*><b>Related Topics:<\\/b>(.*)<\\/span>|',$postCUrlResponse,$matches);
-      if (!$matches){
-        return false;
-      }
-      preg_match_all('|<a href="([^"]*)">(.*)<\\/a>|',$matches[1],$rawLinks);
-      if (!$rawLinks){
-        return false;
-      }
-      $data=array();
-      $x=count($rawLinks[0]);
-      for($i=0;$i<$x;$i++){
-        $data[]=array('url'=>$rawLinks[1][$i],'title'=>$rawLinks[2][$i]);
-      }
-      return $data;
-    }
-
-
-    /**
-     * @param $postCUrlResponse
-     * @return array['title'] ['content'] ['image']
-     */
-    public function parseSidebarAd($postCUrlResponse = null){
-      $dp1=explode('<td id="sidebar"',$postCUrlResponse);
-      if (count($dp1)!=2){
-        echo 'not found sidebar';
-        return false;
-      }
-      else {
-       
-        preg_match('|(<div style="[^"]*">[\\n\\r]*<a href="[^"]*"[^>]*>[\\n\\r]*<div[^>]*><b style="color.*B60505;">([^<\\/b>]*)<\\/b><\\/div>[\\n\\r]*)<div[^>]*>([\\n\\r]*.*)*|', $dp1[1],$matches);
-       
+        preg_match('|<time class="nocontent" datetime="([0-9\-]{10}T[0-9:]{8})" itemprop="datePublished"|', $postCUrlResponse,$matches);
         if ($matches){
-          $data=array('title'=>$matches[2],'content'=>'','image'=>'');
-
-          $dp2=explode($matches[1],$matches[0]);
-          if (count($dp2)==2){
-            preg_match('|<div style="[^"]*">(.*[\\n\\r]*)<\\/div>|', $dp2[1],$matches);
-            if ($matches){
-              $data['content']=$matches[1];
-              preg_match('|<img src="([^"]*)"|',substr($dp2[1],strlen($matches[0])),$matches);
-              if ($matches){
-                $data['image']=$matches[1];
-              }
-              return $data;
-            }
-            else {
-              return false;
-            }
-          }
-          return $data;
+            return $matches[1];
         }
         return false;
-      }
     }
-    
-    public function parserForComments($postCUrlResponse = null){
-      preg_match_all('|<div id="comment_([0-9]*)" class="comment_item">[\\n\\r]*<div[^>]*><b[^>]*>(.*)<\\/b>[^<]*<span[^>]*>(.*)<\\/span><\\/div>[\\n\\r]*<div[^>]*>(.*)<\\/div>|',$postCUrlResponse,$matches);
-      if (!$matches){
+
+    /**
+     * @param $postCUrlResponse
+     */
+    public function parseTopics($postCUrlResponse = null){
+        preg_match('|<span[^>]*><b>Related Topics:<\\/b>(.*)<\\/span>|',$postCUrlResponse,$matches);
+        if (!$matches){
+            return false;
+        }
+        preg_match_all('|<a href="([^"]*)">(.*)<\\/a>|',$matches[1],$rawLinks);
+        if (!$rawLinks){
+            return false;
+        }
+        $data=array();
+        $x=count($rawLinks[0]);
+        for($i=0;$i<$x;$i++){
+            $data[]=array('url'=>$rawLinks[1][$i],'title'=>$rawLinks[2][$i]);
+        }
+        return $data;
+    }
+
+
+    /**
+     * @param $postCUrlResponse
+     */
+    public function parseSidebarAd($postCUrlResponse = null){
+        $dp1=explode('<td id="sidebar"',$postCUrlResponse);
+        if (count($dp1)!=2){
+            echo 'not found sidebar';
+            return false;
+        }
+
+        preg_match('|(<div style="[^"]*">[\\n\\r]*<a href="[^"]*"[^>]*>[\\n\\r]*<div[^>]*><b style="color.*B60505;">([^<\\/b>]*)<\\/b><\\/div>[\\n\\r]*)<div[^>]*>([\\n\\r]*.*)*|', $dp1[1],$matches);
+
+        if ($matches){
+            $data=array('title'=>$matches[2],'content'=>'','image'=>'');
+
+            $dp2=explode($matches[1],$matches[0]);
+            if (count($dp2)==2){
+                preg_match('|<div style="[^"]*">(.*[\\n\\r]*)<\\/div>|', $dp2[1],$matches);
+                if ($matches){
+                    $data['content']=$matches[1];
+                    preg_match('|<img src="([^"]*)"|',substr($dp2[1],strlen($matches[0])),$matches);
+                    if ($matches){
+                        $data['image']=$matches[1];
+                    }
+                    return $data;
+                }
+
+                return false;
+            }
+            return $data;
+        }
         return false;
-      }
-      $data=array();
-      $x=count($matches[0]);
-      for($i=0;$i<$x;$i++){
-        $data[]=array('id'=>$matches[1][$i],'name'=>$matches[2][$i],'date'=>$matches[3][$i],'body'=>$matches[4][$i]);
-      }
-      return $data;
+    }
+
+    public function parserForComments($postCUrlResponse = null){
+        preg_match_all('|<div id="comment_([0-9]*)" class="comment_item">[\\n\\r]*<div[^>]*><b[^>]*>(.*)<\\/b>[^<]*<span[^>]*>(.*)<\\/span><\\/div>[\\n\\r]*<div[^>]*>(.*)<\\/div>|',$postCUrlResponse,$matches);
+        if (!$matches){
+            return false;
+        }
+        $data=array();
+        $x=count($matches[0]);
+        for($i=0;$i<$x;$i++){
+            $data[]=array('id'=>$matches[1][$i],'name'=>$matches[2][$i],'date'=>$matches[3][$i],'body'=>$matches[4][$i]);
+        }
+        return $data;
     }
 }
